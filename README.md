565d9403a8bb1fbbded632da63bb576a45692c26
mixassio <mixail.bondarev@gmail.com>
2017-04-17 11:22:51 +0300

    for first check

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # Github Trends
 
-[TODO. There will be project description]
+The script find repositories on github.com, created at last week  and had better stars. Script shows name repos, count stars and open issues and show links for issues.
+
+# HOW RUN
+
+Example of script launch on Linux, Python 3.6:
+
+```#!bash
+
+$ python github_trending.py 
+--------------------------------------------------
+Name: clac, count stars: 160, count open isues: 3
+Open issues:
+---- https://github.com/soveran/clac/issues/6
+---- https://github.com/soveran/clac/pull/5
+---- https://github.com/soveran/clac/issues/1
+--------------------------------------------------
+Name: dockrails, count stars: 151, count open isues: 0
+NO open issues
+--------------------------------------------------
+```
+
 
 # Project Goals
 
--- github_trending.py
+++ github_trending.py
@@ -2,19 +2,21 @@ import requests, json
 from datetime import datetime, date, time, timedelta
 
 def get_trending_repositories(top_size=20):
-    #get dates for search
     date_today = datetime.date(datetime.now())
     date_before_week = date_today - timedelta(days=7)
-    #get request for github
     date_search = str(date_before_week) + '..' + str(date_today)
     url = 'https://api.github.com/search/repositories?q=created:' + date_search + '&sort=stars'
     params = {'page': 1, 'per_page': top_size}
     repositories = requests.get(url, params)
-    my_dict = repositories.json()
-    for i in my_dict["items"]:
-        print(i['id'], i['stargazers_count'], i['open_issues'], i['name'])
-        if i['open_issues'] > 0:
-            get_open_issues_amount(i['owner'], i['name'])
+    repos = repositories.json()
+    for rep in repos["items"]:
+        print('--------------------------------------------------')
+        print('Name: {}, count stars: {}, count open isues: {}'.format(rep['name'], rep['stargazers_count'], rep['open_issues']))
+        if rep['open_issues'] > 0:
+            print('Open issues:')
+            get_open_issues_amount(rep['owner'], rep['name'])
+        else:
+            print('NO open issues')
 
 
 
@@ -23,12 +25,10 @@ def get_open_issues_amount(repo_owner, repo_name):
     url = 'https://api.github.com/repos/' + repo_owner['login'] + '/' + repo_name +'/issues'
     params = {'page': 1, 'per_page': 20}
     list_issusies = requests.get(url, params)
-    my_dict = list_issusies.json()
-    for i in my_dict:
-        print(i['html_url'])
+    issues = list_issusies.json()
+    for issu in issues:
+        print('----',issu['html_url'])
 
 
 if __name__ == '__main__':
     get_trending_repositories()
-    #repo_owner, repo_name = get_trending_repositories()
-    #get_open_issues_amount(repo_owner, repo_name)
