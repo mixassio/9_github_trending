d1c2ee5d9e952e55506d6b263104d3a850978aa2
mixassio <mixail.bondarev@gmail.com>
2017-04-18 13:07:39 +0300

    add checks params, name, pep8

--- github_trending.py
+++ github_trending.py
@@ -2,33 +2,35 @@ import requests
 import json
 from datetime import datetime, date, time, timedelta
 
-def get_trending_repositories(top_size=20):
+params = {'page': 1, 'per_page': 20}
+
+
+def get_trending_repositories():
     date_today = datetime.date(datetime.now())
     date_before_week = date_today - timedelta(days=7)
     date_search = str(date_before_week) + '..' + str(date_today)
+
     url = 'https://api.github.com/search/repositories?q=created:' + date_search + '&sort=stars'
-    params = {'page': 1, 'per_page': top_size}
     repositories = requests.get(url, params)
+
     repos = repositories.json()
-    for rep in repos["items"]:
+    for new_repo in repos["items"]:
         print('--------------------------------------------------')
-        print('Name: {}, count stars: {}, count open isues: {}'.format(rep['name'], rep['stargazers_count'], rep['open_issues']))
-        if rep['open_issues'] > 0:
+        print('Name: {}, count stars: {}, count open isues: {}'.format(new_repo['name'], new_repo['stargazers_count'], new_repo['open_issues']))
+        if new_repo['open_issues'] > 0:
             print('Open issues:')
-            get_open_issues_amount(rep['owner'], rep['name'])
+            get_open_issues_amount(new_repo['owner'], new_repo['name'])
         else:
             print('NO open issues')
 
 
-
-
 def get_open_issues_amount(repo_owner, repo_name):
     url = 'https://api.github.com/repos/' + repo_owner['login'] + '/' + repo_name +'/issues'
-    params = {'page': 1, 'per_page': 20}
     list_issusies = requests.get(url, params)
+
     issues = list_issusies.json()
-    for issu in issues:
-        print('----',issu['html_url'])
+    for issue in issues:
+        print('----',issue['html_url'])
 
 
 if __name__ == '__main__':
